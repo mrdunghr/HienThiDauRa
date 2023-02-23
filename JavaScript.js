65836642ce2b042dd20f5b064c40ffdeef6373f1
Hà Đạt <mrdunghr>
2023-02-23 11:26:39 +0700

    Thực Hành Hiển Thị Đầu Ra Java Scripts

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+function ShowMessage(){
+    alert('Xin chào!');
+    document.getElementById('message').innerHTML = 'Xin chào!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
+    document.writeln('xin chào');
+    console.log('Xin chào!');
+}
